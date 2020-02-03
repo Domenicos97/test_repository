fd26cb062118229c2776e0d58b796a18d6e2147c
domen <domen@LAPTOP-4HMF1TQR>
2020-02-03 03:26:25 +0100

    add print method

---
+++ EsempioProgetto/src/controller/Film.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class Film {
+	
+	int hello;
+	String ciao;
+	char salut;
+	
+	
+	public void ciao() {
+		System.out.println("Ciao");
+	}
+	
+	public void domy() {
+		System.out.println("domy");
+	}
+	
+	public void print() {
+		System.out.println("print");
+	}
+	
+}
